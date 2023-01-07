0453be4967eab358644610971091da003ae8d319
Almogo97 <alvaromg_97@hotmail.com>
2023-01-07 16:14:14 +0100

    Fix payer rotation not updating unless mouse moved

--- Assets/PlayerAim.cs
+++ Assets/PlayerAim.cs
@@ -5,14 +5,17 @@ public class PlayerAim : MonoBehaviour
 {
     public new Camera camera;
 
+    Vector3 mouseWorldPosition;
+
     public void OnLook(InputValue value)
     {
         Vector2 mouseScreenPosition = value.Get<Vector2>();
-        Vector3 mouseWorldPosition = camera.ScreenToWorldPoint(mouseScreenPosition);
-        //Vector3 targetDirection = mouseWorldPosition - transform.position;
-        //float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        mouseWorldPosition = camera.ScreenToWorldPoint(mouseScreenPosition);
         mouseWorldPosition.z = 0;
+    }
+
+    private void Update()
+    {
         transform.up = mouseWorldPosition - transform.position;
     }
 }
